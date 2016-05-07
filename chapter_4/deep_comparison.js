b7be8fd94a23eb30e3ce86d07fe6499b0f488ecb
Paul Haddad <paulh16@gmail.com>
2016-05-07 09:35:56 -0500

    Chapter 4: Deep Comparison

--- chapter_4/deep_comparison.js
+++ chapter_4/deep_comparison.js
@@ -10,6 +10,36 @@ produces "object" for both values, you should do a deep comparison. But you have
 to take one silly exception into account: by a historical accident, typeof null
 also produces "object". */
 
+function deepEqual(object1, object2) {
+  for (var property in object1)
+  {
+    if (bothAreObjects(object1[property], object2[property]))
+      return deepEqual(object1[property], object2[property]);
+    else
+      return equalProperties(object1[property], object2[property]);
+  }
+}
 
+function equalProperties(property1, property2) {
+  if (property1 === property2)
+    return true;
+  else
+    return false;
+}
 
+function objectsNotNull(object1, object2) {
+  return (object1 != null && object2 != null);
+}
 
+function bothAreObjects(object1, object2)
+{
+  return (typeof(object1) == "object" && typeof(object2) == "object" && objectsNotNull(object1, object2));
+}
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// // → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// // → true
